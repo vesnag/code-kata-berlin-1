232592550d900641ccdc355407d67c31eb37d346
Anil Maharjan <anil@anilmaharjan.com.np>
2019-02-21 15:55:52 +0100

    CKB-1: Code + test

---
+++ index.js
@@ -0,0 +1,17 @@
+/**
+ * @file Index
+ * Code goes here
+ */
+
+
+/**
+ * Sum function
+ * @param a
+ * @param b
+ * @returns {*}
+ */
+function sum(a, b) {
+  return a + b;
+}
+
+module.exports.sum = sum;
