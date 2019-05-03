fad3b39e97748c18366688abd1d395e846df3931
Anil Maharjan <anil@anilmaharjan.com.np>
2019-05-03 12:53:43 +0200

    CKB-1: argument parser

--- javascript/kata.js
+++ javascript/kata.js
@@ -1,5 +1,22 @@
-// function kata(roman, arabic) {
-//     return 1;
-// }
-// 
-// module.exports.kata = kata;
+function parse(argument) {
+  let returnObj = {};
+
+  argument.split(' ').forEach(d => {
+    if (d.indexOf('--') > -1) {
+      if (d.indexOf('=') > -1) {
+        let keyVal = d.split('=');
+        returnObj[keyVal[0].replace('--', '')] = keyVal[1];
+        return;
+      }
+      returnObj[d.replace('--', '')] = true;
+      return;
+    } else if (d.indexOf('-') > -1) {
+      d.replace('-', '').split('').forEach(x => returnObj[x] = true);
+      return;
+    }
+    throw new Error('Invalid Input');
+  });
+  return returnObj
+}
+
+module.exports.kata = parse;
--- javascript/kata.test.js
+++ javascript/kata.test.js
@@ -1,14 +1,27 @@
-// const {kata} = require('./kata');
-// const {each} = require('jest-each');
-// 
-// describe.each([
-// 
-//     [ 'I', 1 ],
-//     [ 'II', 2 ],
-//     [ 'III', 3 ],
-// 
-// ])('returns %s for %s', (roman, arabic) => {
-// 
-//   expect(kata(roman)).toBe(arabic);
-// 
-// });
+const {kata} = require('./kata');
+
+test('it converts short option to bool', () => {
+  expect(kata('-a')).toEqual({a: true});
+});
+test('it converts many short option to bool', () => {
+  expect(kata('-a -b')).toEqual({a: true, b: true});
+});
+test('it converts many short option together to bool', () => {
+  expect(kata('-abc')).toEqual({a: true, b: true, c:true});
+});
+
+test('it converts many short option together to bool', () => {
+  expect(kata('-abc -def')).toEqual({a: true, b: true, c:true, d:true, e:true, f:true});
+});
+
+test('it converts long option to bool', () => {
+  expect(kata('--foo')).toEqual({foo:true});
+});
+
+test('it converts long option to value', () => {
+  expect(kata('--foo=bar')).toEqual({foo:'bar'});
+});
+
+test('it validates input andvalue', () => {
+  expect(() => kata('foo')).toThrow();
+});
