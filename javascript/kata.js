3d76482777ed74abf21994efdf4745c2b7193620
Anil Maharjan <anil@anilmaharjan.com.np>
2019-05-03 14:03:35 +0200

    CKB-1: Dependency Injection

--- javascript/kata.js
+++ javascript/kata.js
@@ -1,5 +1,27 @@
-// function kata(roman, arabic) {
-//     return 1;
-// }
-// 
-// module.exports.kata = kata;
+class Container {
+  constructor() {
+    this.registry = {};
+  }
+
+  register (id, service){
+    this.registry[id] = service(this.registry);
+  }
+
+  get(id) {
+    if (Object.keys(this.registry).indexOf(id) < 0)
+      throw new Error();
+    return this.registry[id];
+  }
+}
+
+class MyService {
+
+}
+
+class DependantService {
+  constructor(MyService) {
+    this.MyService = MyService;
+  }
+}
+
+module.exports = {Container, MyService, DependantService};
