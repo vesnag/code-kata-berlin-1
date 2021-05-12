bfec87987bc6aacb3b56f7e7e16516dd8452fea7
Anil Maharjan <anil@anilmaharjan.com.np>
2021-05-12 16:41:46 +0200

    Fix jest setup

--- javascript/jest.config.js
+++ javascript/jest.config.js
@@ -1,8 +1,5 @@
 module.exports = {
   roots: ['src'],
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest',
-  },
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 };
